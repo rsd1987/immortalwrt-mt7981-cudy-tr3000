0ca81324e62d4a0892f592a55af70c32b5b50acb
rsd1987 <rsd1987@qq.com>
2026-03-24 21:06:53 +0800

    Comment out OpenClash Meta setup commands
    Comment out commands for adding OpenClash Meta.

--- diy-part2.sh
+++ diy-part2.sh
@@ -30,10 +30,10 @@ sed -i -e '/^IMG_PREFIX:=/i BUILD_DATE := $(shell date +%Y%m%d)' \
 # sed -i 's/reg = <0x5c0000 0x7000000>;/reg = <0x5c0000 0x7a40000>;/' target/linux/mediatek/dts/mt7981b-cudy-tr3000-v1-ubootmod.dts
 
 # Add OpenClash Meta
-mkdir -p files/etc/openclash/core
+#mkdir -p files/etc/openclash/core
 
-wget -qO "clash_meta.tar.gz" "https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-arm64.tar.gz"
-tar -zxvf "clash_meta.tar.gz" -C files/etc/openclash/core/
-mv files/etc/openclash/core/clash files/etc/openclash/core/clash_meta
-chmod +x files/etc/openclash/core/clash_meta
-rm -f "clash_meta.tar.gz"
+#wget -qO "clash_meta.tar.gz" "https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-arm64.tar.gz"
+#tar -zxvf "clash_meta.tar.gz" -C files/etc/openclash/core/
+#mv files/etc/openclash/core/clash files/etc/openclash/core/clash_meta
+#chmod +x files/etc/openclash/core/clash_meta
+#rm -f "clash_meta.tar.gz"
